c865e66bbf7a9de40e98e96eaa308ecae6e6c94d
dickk <dihkah121@studentmail.oru.se>
2018-04-12 23:54:50 +0200

    Added a junkprogram TestDsaLabs

--- DsaLabs/DsaLabs/TestDsaLabs.c
+++ DsaLabs/DsaLabs/TestDsaLabs.c
@@ -1,1 +1,24 @@
 //This is a test
+#include <stdio.h>
+#define ARRAY_SIZE 100
+
+int main() {
+	int a[ARRAY_SIZE];
+	int c = 0;
+
+	
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		c++;
+		printf("%d,  ", c);
+	}
+
+	if (c == ARRAY_SIZE)
+	{
+		printf("True! %d    ",c);
+	}
+
+	return 0;
+}
+
+
